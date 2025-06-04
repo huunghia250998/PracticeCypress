6bc9ff16f8160b2c38f3ab3049e188253a51e15b
Nghia <c10huunghia98@gmail.com>
2025-06-04 10:03:38 +0700

    6st

--- cypress/e2e/TC2.cy.js
+++ cypress/e2e/TC2.cy.js
@@ -1,7 +1,7 @@
 import { HomePage } from "../support/pages/homePage"
 
 const homePage = new HomePage()
-describe('Verify click on login and verify it', () => {
+describe('Verify click on login and verify ittttt', () => {
   it('Click on Login', () => {
     cy.visit('https://www.demoblaze.com/')
     homePage.verifyHomePageDisplayed()
